5492b0e71847786e003ff27f937290741ddefe00
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-08-14 23:16:05 +0530

    Create Projects.js

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,14 @@
+// src/pages/Projects.js
+import React from 'react';
+import '../styles/Projects.css';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>Projects</h1>
+      <p>Manage and track your ongoing projects efficiently.</p>
+    </div>
+  );
+}
+
+export default Projects;
